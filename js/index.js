6406ed1c8cef29f4f9b9c7b7fc300fb52102a8c8
fabian <fabiancalderon0499@gmail.com>
2022-08-12 23:38:58 -0500

    Primer punto

--- js/index.js
+++ js/index.js
@@ -1,9 +1,31 @@
-let animal = document.getElementById("animal"); 
-let btnAgregar = document.getElementById("btnAgregar"); 
-let listar = document.getElementById("listado"); 
 
-(function agregaElemento () {
-  btnAgregar.addEventListener("click", () => {
-    listar.innerHTML += `<p>${animal.value}</p>`
-  })
-})()
+
+function add_li(){
+  var entrada = document.getElementById("entrada").value.toLowerCase();
+
+  if(entrada.length>0)
+  {
+      var li=document.createElement('li');
+      li.id=entrada;
+      var text=document.createTextNode(entrada);
+      li.appendChild(text);
+      document.getElementById("lista").appendChild(li);
+     
+    
+  }
+  document.getElementById("entrada").value = "";
+  return false
+
+
+}
+
+function eliminar(){
+  let ele = document.querySelectorAll('li');
+  let cant = ele.length;
+  if (cant > 0){
+    ele = ele[0];
+    ele.remove();
+  }
+ 
+  return false;
+ }
